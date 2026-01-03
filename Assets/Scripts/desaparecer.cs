f2e1eb7f3726833436da91b6199c05bbf8b1b6a3
vivgim <vivesgimenos@gmail.com>
2026-01-03 23:58:25 +0100

    plataformas que desaparecen+ sprites astronautas

---
+++ Assets/Scripts/desaparecer.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class desaparecer : MonoBehaviour
+{
+    public GameObject[] plataformas; // 5 plataformas en orden
+    public float tiempoCambio = 2f;
+
+    float timer;
+    bool imparesActivas = true;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= tiempoCambio)
+        {
+            timer = 0f;
+            imparesActivas = !imparesActivas;
+            ActualizarPlataformas();
+        }
+    }
+
+    void ActualizarPlataformas()
+    {
+        
+        plataformas[0].SetActive(imparesActivas);
+        plataformas[2].SetActive(imparesActivas);
+        plataformas[4].SetActive(imparesActivas);
+
+        
+        plataformas[1].SetActive(!imparesActivas);
+        plataformas[3].SetActive(!imparesActivas);
+    }
+}
