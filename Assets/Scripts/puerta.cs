a48d5702a8ea75e0b947cb9fddc89d3cc3ad03b5
vivgim <vivesgimenos@gmail.com>
2026-01-03 12:34:29 +0100

    pantalla win funciona con todos los players

--- Assets/Scripts/puerta.cs
+++ Assets/Scripts/puerta.cs
@@ -7,13 +7,8 @@ public class puerta : NetworkBehaviour
     public GameObject puertaCerrada;
     public GameObject pantallaWin;
 
-    
+    private NetworkVariable<bool> jugadorEnTrigger = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone,NetworkVariableWritePermission.Server);
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     
     void OnTriggerEnter2D(Collider2D other)
@@ -21,45 +16,77 @@ public class puerta : NetworkBehaviour
         if (!IsServer) return;
         if (other.CompareTag("Player"))
         {
-            Players jugador = other.GetComponent<Players>();
+           Players jugador = other.GetComponent<Players>();
+            
+            
+
             
             if (jugador != null && jugador.tieneCombustible)
             {
+                jugadorEnTrigger.Value = true;
                 DesactivarPuertaClientRpc();
-
             }
 
 
         }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Players jugador = other.GetComponent<Players>();
+        //if (jugador != null && jugador.IsOwner)
+        if(other.CompareTag("Player"))
+        {
+            jugadorEnTrigger.Value = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (!IsClient) return;
+       
+        if (Input.GetKeyDown(KeyCode.W) && jugadorEnTrigger.Value)
+        {
+            WinServerRpc();
+        }
+    }
+
+
+
+
+
+  /* void OnTriggerStay2D(Collider2D other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            Players jugador = other.GetComponent<Players>();
+            //if (jugador.tieneCombustible && Input.GetKeyDown(KeyCode.W))
+            if (jugador.tieneCombustible) 
+            {
+                //PantallaWinClientRpc();
+                jugadorEnTrigger = true;
+            }
+        }
+    }
+  */
+
+    [Rpc(SendTo.Server, InvokePermission = RpcInvokePermission.Everyone)]
+    void WinServerRpc()
+    {
+        if(jugadorEnTrigger.Value)
+        PantallaWinClientRpc();
     }
 
     [ClientRpc]
     void DesactivarPuertaClientRpc()
     {
-            puertaCerrada.SetActive(false);
-
-    
+        puertaCerrada.SetActive(false);
     }
 
-
-    
-    void OnTriggerStay2D(Collider2D other)
-    {
-        if (other.CompareTag("Player") && other.GetComponent<Players>().IsOwner)
-        {
-            Players jugador = other.GetComponent<Players>();
-            if (jugador.tieneCombustible && Input.GetKeyDown(KeyCode.W))
-            {
-                PantallaWinClientRpc();
-
-            }
-        }
-    }
-    
     [ClientRpc]
     void PantallaWinClientRpc()
     {
-
             pantallaWin.SetActive(true);
     }
 
