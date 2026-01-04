1e7682ba88ce5a91a284c679551c958d42042f76
laura <lauraboadella@gmail.com>
2026-01-04 01:10:21 +0100

    boton puerta a medias

---
+++ Assets/Scripts/botonPuerta.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using Unity.Netcode;
+
+
+public class botonPuerta : MonoBehaviour
+{
+    public GameObject botonAbierto;
+    public GameObject botonCerrado;
+    
+    public bool botonActivado = false;
+
+    public enum TipoBoton
+    {
+        Plataforma,
+        Pinchos
+    }
+    public TipoBoton tipoboton;
+
+    public GameObject plataforma;
+    private Animator animPlataforma;
+
+    public GameObject pinchosmortales;
+    //public pincho pincho;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animPlataforma = plataforma.GetComponent<Animator>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ActivarBoton(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ActivarBoton(false);
+        }
+
+    }
+
+
+    void ActivarBoton(bool mover)
+    {
+        botonActivado = mover;
+
+        if (botonAbierto != null) botonAbierto.SetActive(!mover);
+        if (botonCerrado != null) botonCerrado.SetActive(mover);
+
+        switch (tipoboton)
+        {
+            case TipoBoton.Plataforma:
+                if (animPlataforma != null)
+                    animPlataforma.SetBool("Mover", mover);
+                break;
+
+            case TipoBoton.Pinchos:
+                pinchosmortales.SetActive(true);
+                break;
+        }
+    }
+
+}
